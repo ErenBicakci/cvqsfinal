6dda96d85ca8390cabf279022ff0152d6a9a013c
Eren <eren.bicakci2003@gmail.com>
2023-02-24 21:39:50 +0300

    1.0.0 Project Start

---
+++ src/main/java/com/toyota/cvqsfinal/CvqsfinalApplication.java
@@ -0,0 +1,13 @@
+package com.toyota.cvqsfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvqsfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvqsfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyota/cvqsfinal/CvqsfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyota.cvqsfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvqsfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
