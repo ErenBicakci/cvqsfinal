58cb18f90baa1a761d101c783ba4118b339abdb2
Eren <eren.bicakci2003@gmail.com>
2023-03-03 23:28:42 +0300

    2.1.1 - Improvements in logging

--- src/main/java/com/toyota/cvqsfinal/controller/VehicleDefectController.java
+++ src/main/java/com/toyota/cvqsfinal/controller/VehicleDefectController.java
@@ -3,6 +3,7 @@ package com.toyota.cvqsfinal.controller;
 import com.toyota.cvqsfinal.dto.DefectDto;
 import com.toyota.cvqsfinal.dto.VehicleDefectDto;
 import com.toyota.cvqsfinal.dto.VehicleDto;
+import com.toyota.cvqsfinal.service.JwtService;
 import com.toyota.cvqsfinal.service.VehicleDefectService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -16,9 +17,13 @@ import org.springframework.web.bind.annotation.*;
 public class VehicleDefectController {
 
     private final VehicleDefectService vehicleDefectService;
+    private final JwtService jwtService;
+
 
     @PostMapping("/save/{vehicleId}/{defectId}")
-    ResponseEntity<VehicleDefectDto> saveVehicleDefect(@PathVariable Long vehicleId, @RequestBody VehicleDefectDto vehicleDefectDto)throws Exception{
+    ResponseEntity<VehicleDefectDto> saveVehicleDefect(@PathVariable Long vehicleId, @RequestBody VehicleDefectDto vehicleDefectDto,@RequestHeader (name="Authorization") String token)throws Exception{
+        log.info(jwtService.findUsername(token.substring(7))+" VehicleDefect Saved");
+
         VehicleDefectDto vehicleDefectDto1 = vehicleDefectService.vehicleDefectSave(vehicleId,vehicleDefectDto);
         if (vehicleDefectDto1 != null){
             return ResponseEntity.ok(vehicleDefectDto1);
@@ -28,28 +33,40 @@ public class VehicleDefectController {
 
 
     @DeleteMapping("/delete/{vehicleDefectId}")
-    void saveVehicleDefect(@PathVariable Long vehicleDefectId)throws Exception{
+    void deleteVehicleDefect(@PathVariable Long vehicleDefectId,@RequestHeader (name="Authorization") String token){
+        log.info(jwtService.findUsername(token.substring(7))+" Send VehicleDefect delete request : (VEHICLEDEFECT CODE) " + vehicleDefectId);
+
         boolean vehicleDto = vehicleDefectService.vehicleDefectDel(vehicleDefectId);
         if (vehicleDto){
             return;
         }
+        log.error(jwtService.findUsername(token.substring(7))+" VEHICLEDEFECT NOT FOUND : (VEHICLE CODE) " + vehicleDefectId);
+
+
     }
 
     @PutMapping("/update")
-    ResponseEntity<VehicleDefectDto> updateVehicleDefect(@RequestBody VehicleDefectDto vehicleDefectDto)throws Exception{
+    ResponseEntity<VehicleDefectDto> updateVehicleDefect(@RequestBody VehicleDefectDto vehicleDefectDto,@RequestHeader (name="Authorization") String token){
+        log.info(jwtService.findUsername(token.substring(7))+"Send VehicleDefect update request : (VEHICLE CODE) " + vehicleDefectDto.getId());
+
         VehicleDefectDto vehicleDefectDto1 = vehicleDefectService.vehicleDefectUpdate(vehicleDefectDto);
         if (vehicleDefectDto1 != null){
             return ResponseEntity.ok(vehicleDefectDto1);
         }
+        log.error(jwtService.findUsername(token.substring(7))+" VEHICLEDEFECT NOT FOUND : (VEHICLE CODE) " + vehicleDefectDto.getId());
+
         return ResponseEntity.status(400).body(null);
     }
 
     @GetMapping("/get/{vehicleDefectId}")
-    ResponseEntity<VehicleDefectDto> getVehicleDefect(@PathVariable Long vehicleDefectId)throws Exception{
+    ResponseEntity<VehicleDefectDto> getVehicleDefect(@PathVariable Long vehicleDefectId,@RequestHeader (name="Authorization") String token){
+        log.error(jwtService.findUsername(token.substring(7))+" GET VEHICLEDEFECT : (VEHICLE CODE) " + vehicleDefectId);
+
         VehicleDefectDto vehicleDefectDto = vehicleDefectService.vehicleDefectGet(vehicleDefectId);
         if (vehicleDefectDto != null){
             return ResponseEntity.ok(vehicleDefectDto);
         }
+        log.error(jwtService.findUsername(token.substring(7))+" VEHICLEDEFECT NOT FOUND : (VEHICLE CODE) " + vehicleDefectDto.getId());
         return ResponseEntity.status(400).body(null);
     }
 }
