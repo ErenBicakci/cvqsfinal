944bc7b526f2ca0006261391cd0f8ec57bdc3c14
Eren <eren.bicakci2003@hotmail.com>
2023-05-27 08:24:31 +0300

    4.1.0 Add Eureka Server
    -Communication between containers now takes place via eureka server

--- Main/src/main/java/com/toyota/cvqsfinal/CvqsfinalApplication.java
+++ Main/src/main/java/com/toyota/cvqsfinal/CvqsfinalApplication.java
@@ -2,10 +2,12 @@ package com.toyota.cvqsfinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CvqsfinalApplication {
 
     public static void main(String[] args) {
---
+++ eurekaserver/src/test/java/com/deneme/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,11 @@
+package src.test.java.com.deneme.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+
+
+}
